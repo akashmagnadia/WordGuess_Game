ab70948396613ede90a537f2af2e962dc6d5d19f
Akash Magnadia <akash.magnadia@gmail.com>
2020-07-14 22:34:37 -0500

    Integrated Datamuse API into the Game

--- P4Server/src/main/java/ServerSideGameInfo.java
+++ P4Server/src/main/java/ServerSideGameInfo.java
@@ -5,6 +5,7 @@ import datamuse.query.DatamuseQuery;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Random;
 
 public class ServerSideGameInfo implements Serializable {
     int clientNumber = -1;
@@ -12,34 +13,26 @@ public class ServerSideGameInfo implements Serializable {
     String foodCategory = "Food";
     String statesCategory = "States";
 
-    DatamuseCaller caller = new DatamuseCaller();
-    DatamuseQuery query = (new DatamuseQuery())
-            .relatedAdjectiveFor("water")
-            .withinTopicHint("ocean","temperature")
-            .startingWith("s");
-    List<DatamuseWord> words = caller.call(query);
+    String animalsCategory_WordOne;
+    char[] animalsCategory_WordOneArray;
+    String animalsCategory_WordTwo;
+    char[] animalsCategory_WordTwoArray;
+    String animalsCategory_WordThree;
+    char[] animalsCategory_WordThreeArray;
 
-    //as the user guesses the word, the char array will be filled with the letters
-    String animalsCategory_WordOne = "cheetah".toLowerCase();
-    char[] animalsCategory_WordOneArray = new char[animalsCategory_WordOne.length()];
-    String animalsCategory_WordTwo = "lion".toLowerCase();
-    char[] animalsCategory_WordTwoArray = new char[animalsCategory_WordTwo.length()];
-    String animalsCategory_WordThree = "tiger".toLowerCase();
-    char[] animalsCategory_WordThreeArray = new char[animalsCategory_WordThree.length()];
+    String foodCategory_WordOne;
+    char[] foodCategory_WordOneArray;
+    String foodCategory_WordTwo;
+    char[] foodCategory_WordTwoArray;
+    String foodCategory_WordThree;
+    char[] foodCategory_WordThreeArray;
 
-    String foodCategory_WordOne = "fries".toLowerCase();
-    char[] foodCategory_WordOneArray = new char[foodCategory_WordOne.length()];
-    String foodCategory_WordTwo = "pancake".toLowerCase();
-    char[] foodCategory_WordTwoArray = new char[foodCategory_WordTwo.length()];
-    String foodCategory_WordThree = "pizza".toLowerCase();
-    char[] foodCategory_WordThreeArray = new char[foodCategory_WordThree.length()];
-
-    String statesCategory_WordOne = "i llinois".toLowerCase();
-    char[] statesCategory_WordOneArray = new char[statesCategory_WordOne.length()];
-    String statesCategory_WordTwo = "f lorida".toLowerCase();
-    char[] statesCategory_WordTwoArray = new char[statesCategory_WordTwo.length()];
-    String statesCategory_WordThree = "w isconsin".toLowerCase();
-    char[] statesCategory_WordThreeArray = new char[statesCategory_WordThree.length()];
+    String statesCategory_WordOne;
+    char[] statesCategory_WordOneArray;
+    String statesCategory_WordTwo;
+    char[] statesCategory_WordTwoArray;
+    String statesCategory_WordThree;
+    char[] statesCategory_WordThreeArray;
 
     Boolean playingAnimalsCategory = false;
     Boolean animalsCategorySolved = false;
@@ -77,5 +70,179 @@ public class ServerSideGameInfo implements Serializable {
     int attempts = 0;
 
     public ServerSideGameInfo() throws IOException {
+        //credit to https://github.com/twinters/datamuse-java for implementation of datamuse API
+        DatamuseCaller caller = new DatamuseCaller();
+        DatamuseQuery animalQuery = (new DatamuseQuery())
+                .withinTopicHint("animal","mammal");
+        List<DatamuseWord> animalWords = caller.call(animalQuery);
+        //less than 11 letter
+
+        DatamuseQuery foodQuery = (new DatamuseQuery())
+                .withinTopicHint("food","snack","dinner","lunch");
+        List<DatamuseWord> foodWords = caller.call(foodQuery);
+        //more than 3 and less than 11
+
+        DatamuseQuery stateQuery = (new DatamuseQuery())
+                .withinTopicHint("state","United States");
+        List<DatamuseWord> stateWords = caller.call(stateQuery);
+        //chose until 43 and size less than 11
+        //size - 57 for random function that would be the max and min is of course 0
+
+        //think for case where no internet therefore list is empty have default word to take over
+
+        if (animalWords.size() > 0) {
+            while (animalsCategory_WordOne == null && animalsCategory_WordTwo == null && animalsCategory_WordThree == null) {
+                if (animalsCategory_WordOne == null) {
+                    int randomNum = getRandomInRange(0, animalWords.size());
+                    String wordPick = animalWords.get(randomNum).getWord();
+                    if (!(wordPick.length() > 10)) {
+                        if (!((animalsCategory_WordTwo != null && animalsCategory_WordTwo.equals(wordPick)) ||
+                                (animalsCategory_WordThree != null && animalsCategory_WordThree.equals(wordPick)))) {
+                            //make sure other two doesn't have the same word
+                            animalsCategory_WordOne = animalWords.get(randomNum).getWord().toLowerCase();
+                            animalsCategory_WordOneArray = new char[animalsCategory_WordOne.length()];
+                        }
+                    }
+                }
+
+                if (animalsCategory_WordTwo == null) {
+                    int randomNum = getRandomInRange(0, animalWords.size());
+                    String wordPick = animalWords.get(randomNum).getWord();
+                    if (!(wordPick.length() > 10)) {
+                        if (!((animalsCategory_WordOne != null && animalsCategory_WordOne.equals(wordPick)) ||
+                                (animalsCategory_WordThree != null && animalsCategory_WordThree.equals(wordPick)))) {
+                            //make sure other two doesn't have the same word
+                            animalsCategory_WordTwo = animalWords.get(randomNum).getWord().toLowerCase();
+                            animalsCategory_WordTwoArray = new char[animalsCategory_WordTwo.length()];
+                        }
+                    }
+                }
+
+                if (animalsCategory_WordThree == null) {
+                    int randomNum = getRandomInRange(0, animalWords.size());
+                    String wordPick = animalWords.get(randomNum).getWord();
+                    if (!(wordPick.length() > 10)) {
+                        if (!((animalsCategory_WordOne != null && animalsCategory_WordOne.equals(wordPick)) ||
+                                (animalsCategory_WordTwo != null && animalsCategory_WordTwo.equals(wordPick)))) {
+                            //make sure other two doesn't have the same word
+                            animalsCategory_WordThree = animalWords.get(randomNum).getWord().toLowerCase();
+                            animalsCategory_WordThreeArray = new char[animalsCategory_WordThree.length()];
+                        }
+                    }
+                }
+            }
+        } else {
+            //if there is no internet then it defaults
+            animalsCategory_WordOne = "cheetah".toLowerCase();
+            animalsCategory_WordOneArray = new char[animalsCategory_WordOne.length()];
+            animalsCategory_WordTwo = "lion".toLowerCase();
+            animalsCategory_WordTwoArray = new char[animalsCategory_WordTwo.length()];
+            animalsCategory_WordThree = "tiger".toLowerCase();
+            animalsCategory_WordThreeArray = new char[animalsCategory_WordThree.length()];
+        }
+
+        if (foodWords.size() > 0) {
+            while (foodCategory_WordOne == null && foodCategory_WordTwo == null && foodCategory_WordThree == null) {
+                if (foodCategory_WordOne == null) {
+                    int randomNum = getRandomInRange(0, foodWords.size());
+                    String wordPick = foodWords.get(randomNum).getWord();
+                    if (!(wordPick.length() > 10 || wordPick.length() < 4)) {
+                        if (!((foodCategory_WordTwo != null && foodCategory_WordTwo.equals(wordPick)) ||
+                                (foodCategory_WordThree != null && foodCategory_WordThree.equals(wordPick)))) {
+                            //make sure other two doesn't have the same word
+                            foodCategory_WordOne = foodWords.get(randomNum).getWord().toLowerCase();
+                            foodCategory_WordOneArray = new char[foodCategory_WordOne.length()];
+                        }
+                    }
+                }
+
+                if (foodCategory_WordTwo == null) {
+                    int randomNum = getRandomInRange(0, foodWords.size());
+                    String wordPick = foodWords.get(randomNum).getWord();
+                    if (!(wordPick.length() > 10 || wordPick.length() < 4)) {
+                        if (!((foodCategory_WordOne != null && foodCategory_WordOne.equals(wordPick)) ||
+                                (foodCategory_WordThree != null && foodCategory_WordThree.equals(wordPick)))) {
+                            //make sure other two doesn't have the same word
+                            foodCategory_WordTwo = foodWords.get(randomNum).getWord().toLowerCase();
+                            foodCategory_WordTwoArray = new char[foodCategory_WordTwo.length()];
+                        }
+                    }
+                }
+
+                if (foodCategory_WordThree == null) {
+                    int randomNum = getRandomInRange(0, foodWords.size());
+                    String wordPick = foodWords.get(randomNum).getWord();
+                    if (!(wordPick.length() > 10 || wordPick.length() < 4)) {
+                        if (!((foodCategory_WordTwo != null && foodCategory_WordTwo.equals(wordPick)) ||
+                                (foodCategory_WordOne != null && foodCategory_WordOne.equals(wordPick)))) {
+                            //make sure other two doesn't have the same word
+                            foodCategory_WordThree = foodWords.get(randomNum).getWord().toLowerCase();
+                            foodCategory_WordThreeArray = new char[foodCategory_WordThree.length()];
+                        }
+                    }
+                }
+            }
+        } else {
+            foodCategory_WordOne = "fries".toLowerCase();
+            foodCategory_WordOneArray = new char[foodCategory_WordOne.length()];
+            foodCategory_WordTwo = "pancake".toLowerCase();
+            foodCategory_WordTwoArray = new char[foodCategory_WordTwo.length()];
+            foodCategory_WordThree = "pizza".toLowerCase();
+            foodCategory_WordThreeArray = new char[foodCategory_WordThree.length()];
+        }
+
+        if (stateWords.size() > 0) {
+            while (statesCategory_WordOne == null && statesCategory_WordTwo == null && statesCategory_WordThree == null) {
+                if (statesCategory_WordOne == null) {
+                    int randomNum = getRandomInRange(0, 43);
+                    String wordPick = stateWords.get(randomNum).getWord();
+                    if (!(wordPick.length() > 10 || wordPick.length() < 4)) {
+                        if (!((statesCategory_WordTwo != null && statesCategory_WordTwo.equals(wordPick)) ||
+                                (statesCategory_WordThree != null && statesCategory_WordThree.equals(wordPick)))) {
+                            //make sure other two doesn't have the same word
+                            statesCategory_WordOne = stateWords.get(randomNum).getWord().toLowerCase();
+                            statesCategory_WordOneArray = new char[statesCategory_WordOne.length()];
+                        }
+                    }
+                }
+
+                if (statesCategory_WordTwo == null) {
+                    int randomNum = getRandomInRange(0, 43);
+                    String wordPick = stateWords.get(randomNum).getWord();
+                    if (!(wordPick.length() > 10 || wordPick.length() < 4)) {
+                        if (!((statesCategory_WordThree != null && statesCategory_WordThree.equals(wordPick)) ||
+                                (statesCategory_WordOne != null && statesCategory_WordOne.equals(wordPick)))) {
+                            //make sure other two doesn't have the same word
+                            statesCategory_WordTwo = stateWords.get(randomNum).getWord().toLowerCase();
+                            statesCategory_WordTwoArray = new char[statesCategory_WordTwo.length()];
+                        }
+                    }
+                }
+
+                if (statesCategory_WordThree == null) {
+                    int randomNum = getRandomInRange(0, 43);
+                    String wordPick = stateWords.get(randomNum).getWord();
+                    if (!(wordPick.length() > 10)) {
+                        if (!((statesCategory_WordTwo != null && statesCategory_WordTwo.equals(wordPick)) ||
+                                (statesCategory_WordOne != null && statesCategory_WordOne.equals(wordPick)))) {
+                            //make sure other two doesn't have the same word
+                            statesCategory_WordThree = stateWords.get(randomNum).getWord().toLowerCase();
+                            statesCategory_WordThreeArray = new char[statesCategory_WordThree.length()];
+                        }
+                    }
+                }
+            }
+        } else {
+            statesCategory_WordOne = "illinois".toLowerCase();
+            statesCategory_WordOneArray = new char[statesCategory_WordOne.length()];
+            statesCategory_WordTwo = "florida".toLowerCase();
+            statesCategory_WordTwoArray = new char[statesCategory_WordTwo.length()];
+            statesCategory_WordThree = "wisconsin".toLowerCase();
+            statesCategory_WordThreeArray = new char[statesCategory_WordThree.length()];
+        }
+    }
+
+    private int getRandomInRange(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 }
