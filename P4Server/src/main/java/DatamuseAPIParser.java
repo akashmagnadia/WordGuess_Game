136809f6dd8d97a7a6000eff827a6bf964de779d
Akash <akash.magnadia@gmail.com>
2021-03-07 20:40:37 -0600

    Code cleanup

--- P4Server/src/main/java/DatamuseAPIParser.java
+++ P4Server/src/main/java/DatamuseAPIParser.java
@@ -5,8 +5,6 @@ import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Scanner;
 
